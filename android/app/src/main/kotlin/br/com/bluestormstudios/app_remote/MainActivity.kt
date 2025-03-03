bd71b08417515d8a701f33f8fce0ae31ae704026
Mario Moraes <moraes_mario@hotmail.com>
2025-03-03 10:11:09 -0300

    Changes

--- android/app/src/main/kotlin/br/com/bluestormstudios/app_remote/MainActivity.kt
+++ android/app/src/main/kotlin/br/com/bluestormstudios/app_remote/MainActivity.kt
@@ -1,16 +1,19 @@
 package br.com.bluestormstudios.app_remote
 
 import io.flutter.embedding.android.FlutterActivity
-import io.flutter.embedding.engine.plugins.FlutterPlugin
+import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import android.content.Intent
 import android.os.Bundle
+import android.view.Surface
+import io.flutter.embedding.engine.renderer.FlutterRenderer
 
 class MainActivity : FlutterActivity() {
     private val CHANNEL = "scrcpy_channel"
+    private var textureId: Long? = null
 
-    override fun configureFlutterEngine(flutterEngine: FlutterPlugin.FlutterEngine) {
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
         super.configureFlutterEngine(flutterEngine)
         MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler { call, result ->
             when (call.method) {
@@ -18,13 +21,24 @@ class MainActivity : FlutterActivity() {
                     val serverAdr = call.argument<String>("serverAdr") ?: ""
                     val videoBitrate = call.argument<Int>("videoBitrate") ?: 8000000
                     val maxHeight = call.argument<Int>("maxHeight") ?: 1920
+                    
+                    // Criar uma Texture para o Flutter
+                    textureId = flutterEngine.renderer.createTexture()
+                    val surfaceTexture = flutterEngine.renderer.getSurfaceTexture(textureId!!)
+                    val surface = Surface(surfaceTexture)
+
                     val intent = Intent(this, Scrcpy::class.java).apply {
                         putExtra("serverAdr", serverAdr)
                         putExtra("videoBitrate", videoBitrate)
                         putExtra("maxHeight", maxHeight)
                     }
                     startService(intent)
-                    result.success("Scrcpy iniciado")
+                    
+                    // Iniciar o Scrcpy com a Surface
+                    val scrcpy = Scrcpy()
+                    scrcpy.start(surface, serverAdr, maxHeight, videoBitrate)
+
+                    result.success(textureId)
                 }
                 else -> result.notImplemented()
             }
